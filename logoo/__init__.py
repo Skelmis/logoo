75d26647c669d0c33d37144f33ca72fa8c6249fb
skelmis <ethanmkh2@gmail.com>
2024-03-04 19:22:10 +1300

    chore: deconstrain version reqs

--- logoo/__init__.py
+++ logoo/__init__.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 from .logger import Logger
 from .primary_logger import PrimaryLogger
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel="final", serial=0)
+version_info = VersionInfo(major=1, minor=0, micro=1, releaselevel="final", serial=0)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
