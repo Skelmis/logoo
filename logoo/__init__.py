6c196c813e2b64960bb7b56514f25a387f0f3d10
skelmis <ethanmkh2@gmail.com>
2024-06-13 21:40:11 +1200

    feat: loosen version requirements

--- logoo/__init__.py
+++ logoo/__init__.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 from .logger import Logger
 from .primary_logger import PrimaryLogger
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=5, micro=0, releaselevel="final", serial=0)
+version_info = VersionInfo(major=1, minor=5, micro=1, releaselevel="final", serial=0)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
